2419c3a353c34946bf91ba077d32e4e4edfaa370
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-08-23 03:07:10 +0300

    add edge cases

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-UFT8 Validation.
+UTF-8 Validation.
 """
 
 
 def validUTF8(data):
-    """check if data is utf 8 valid"""
+    """Check if data is a valid UTF-8 encoding."""
     num_bytes = 0
     mask1 = 0b10000000
     mask2 = 0b11000000
@@ -14,12 +14,22 @@ def validUTF8(data):
         if num_bytes == 0:
             if (num & mask1) == 0:
                 continue
-            elif (num & mask2) == 0b11000000:
+            elif (num & 0b11100000) == 0b11000000:
                 num_bytes = 1
-            elif (num & 0b11100000) == 0b11100000:
+                if num < 0b11000010:
+                    return False
+            elif (num & 0b11110000) == 0b11100000:
                 num_bytes = 2
-            elif (num & 0b11110000) == 0b11110000:
+                if num == 0b11100000 and (data[1] & mask1) == 0:
+                    return False
+                if num == 0b11101101 and (data[1] & 0b11100000) == 0b10100000:
+                    return False
+            elif (num & 0b11111000) == 0b11110000:
                 num_bytes = 3
+                if num == 0b11110000 and (data[1] & 0b11110000) == 0:
+                    return False
+                if num > 0b11110100:
+                    return False
             else:
                 return False
         else:
